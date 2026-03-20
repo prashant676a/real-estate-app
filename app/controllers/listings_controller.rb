2316af07c377acc2c0aadf4a2083056ad00a227d
prashant676a <prashantbhandari.374@gmail.com>
2026-03-20 17:30:53 +0545

    add basic search functionality

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,34 @@
 class ListingsController < ApplicationController
   def index
     @properties = Property.all
+  
+    if params[:price_min].present?
+      @properties = @properties.where("price >= ?", params[:price_min])
+    end
+  
+    if params[:price_max].present?
+      @properties = @properties.where("price <= ?", params[:price_max])
+    end
+  
+    if params[:beds].present?
+      @properties = @properties.where(beds: params[:beds])
+    end
+  
+    if params[:property_type].present?
+      @properties = @properties.where(property_type: params[:property_type])
+    end
+  
+    if params[:suburb].present?
+      @properties = @properties.where("suburb ILIKE ?", "%#{params[:suburb]}%")
+    end
+  
+    if params[:keyword].present?
+      @properties = @properties.where(
+        "title ILIKE ? OR description ILIKE ?",
+        "%#{params[:keyword]}%",
+        "%#{params[:keyword]}%"
+      )
+    end
   end
 
   def show
