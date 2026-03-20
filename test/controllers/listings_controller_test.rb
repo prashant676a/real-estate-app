bdcb5297dc94834a3546d094937ae9211d5b36ef
prashant676a <prashantbhandari.374@gmail.com>
2026-03-20 18:40:05 +0545

    add minimal testing

--- test/controllers/listings_controller_test.rb
+++ test/controllers/listings_controller_test.rb
@@ -1,13 +1,20 @@
 require "test_helper"
 
 class ListingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get listings_index_url
-    assert_response :success
+  setup do
+    @property = Property.create!(title: "Sample House", price: 400000, beds: 2, baths: 1, property_type: "house", suburb: "Northside")
   end
 
-  test "should get show" do
-    get listings_show_url
+  test "should get index" do
+    get listings_url
     assert_response :success
+    assert_select "div", /Sample House/
+  end
+
+  test "admin view shows internal notes" do
+    @property.update(internal_notes: "Test note")
+    get listings_url(is_admin: true)
+    assert_response :success
+    assert_select "p", /Test note/
   end
 end
--- test/models/property_test.rb
+++ test/models/property_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class PropertyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create property" do
+    property = Property.new(title: "Test House", price: 500000, beds: 3, baths: 2, property_type: "apartment", suburb: "Northside")
+    assert property.save, "Property did not save"
+  end
+
+  test "should not save without title" do
+    property = Property.new(price: 500000)
+    assert_not property.save, "Saved property without title"
+  end
 end
